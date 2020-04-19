815f7322b1ac615a09194d5bde92bf615e774bf2
Tinend <ulrich.brodowsky@gmx.net>
2020-04-19 18:54:59 +0200

    Tier geschrieben

---
+++ src/Tier.java
@@ -0,0 +1,15 @@
+public class Tier {
+    private final int groesse;
+    private int energie;
+
+    public Tier(int groesse, int energie) {
+        this.groesse = groesse;
+        this.energie = energie;
+    }
+
+    public Tier kindZeugen() {
+        int energieVerlust = this.energie / 2;
+        this.energie -= energieVerlust;
+        return new Tier(groesse, energieVerlust);
+    }
+}
