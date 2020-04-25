87cfe7cfccc765f6cd96870add7619909369590d
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-04-25 18:01:08 +0200

    Added Postgresql Library, first script to populate DB and first Java code that accesses it.

---
+++ out/production/Evo/sql/create_test_table.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS tier_arten;
+
+CREATE TABLE IF NOT EXISTS tier_arten (
+    id serial PRIMARY KEY,
+    name VARCHAR(25)
+);
+
+INSERT INTO tier_arten(name) VALUES('Tiger');
+INSERT INTO tier_arten(name) VALUES('Wolf');
